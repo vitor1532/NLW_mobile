12598cc291b2980f10514351f725a125afcd28aa
Vitor <vitomartinsalmeida@gmail.com>
2022-09-17 19:54:47 -0300

    configurando relacionamento com backend e criando rota de cada game

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -6,9 +6,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number
+    };
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -21,7 +23,7 @@ export function GameCard({ data, ...rest }: Props) {
 
         <ImageBackground 
             style={styles.cover}
-            source={data.cover}
+            source={{uri: data.bannerUrl}}
         >
 
             <LinearGradient
@@ -29,11 +31,11 @@ export function GameCard({ data, ...rest }: Props) {
                 style={styles.footer}
             >
                 <Text style={styles.name}>
-                    {data.name}
+                    {data.title}
                 </Text>
 
                 <Text style={styles.ads}>
-                    {data.ads} anúncios
+                    {data._count.ads} anúncios
                 </Text>
             </LinearGradient>
         </ImageBackground>
